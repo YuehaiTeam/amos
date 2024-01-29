03db0f81e3f30a1dc5e03c6d36287a9c5ad55ac3
xyToki <7547189+xytoki@users.noreply.github.com>
2024-01-29 13:11:12 +0800

    fix: type

--- src/achievement/index.ts
+++ src/achievement/index.ts
@@ -8,7 +8,7 @@ import { giAchievement, giAchievementGoal } from '../typings/giAchievement'
 import { textMap, checkTextExist, loadCachedText, checkTextIncludesTest } from '../utils/textMap'
 import { keyPair, reKey } from '../utils/bruteforceJson'
 import { giMainQuest } from '../typings/giQuest'
-const camelToSnake = str => str.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
+const camelToSnake = (str:string):string => str.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
 const deprecatedIds = [
     // dreprecated
     84027, 82011, 82016, 82018, 84517, 84521, 81006, 81007, 81008, 81009, 81011, 81012, 81013, 81219,
